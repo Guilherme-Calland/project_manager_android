6070d9092afd25be8407890dc70f24847d504a44
guilhermow <gui.crb0@gmail.com>
2022-01-28 13:15:28 -0300

    creating and reading members

--- app/src/main/java/com/guilhermecallandprojects/projectmanager/screens/MembersActivity.kt
+++ app/src/main/java/com/guilhermecallandprojects/projectmanager/screens/MembersActivity.kt
@@ -3,8 +3,17 @@ package com.guilhermecallandprojects.projectmanager.screens
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
+import android.view.View.*
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.SearchView
 import android.widget.Toast
+import androidx.core.view.isGone
 import androidx.core.view.setPadding
 import com.guilhermecallandprojects.projectmanager.R
 import com.guilhermecallandprojects.projectmanager.adapters.MemberAdapter
@@ -12,6 +21,11 @@ import com.guilhermecallandprojects.projectmanager.database.MembersDatabaseHelpe
 import com.guilhermecallandprojects.projectmanager.model.Member
 import com.guilhermecallandprojects.projectmanager.utils.Util
 import kotlinx.android.synthetic.main.activity_members.*
+import kotlinx.android.synthetic.main.member.*
+import android.widget.LinearLayout
+
+
+
 
 class MembersActivity : AppCompatActivity() {
     private lateinit var membersList: ArrayList<Member>
@@ -25,6 +39,42 @@ class MembersActivity : AppCompatActivity() {
         setContentView(R.layout.activity_members)
         setActionBarProperties()
         initializeMemberList()
+        setNameEditTextListeners()
+    }
+
+    private fun setNameEditTextListeners() {
+        disableFullscreen(et_member_name)
+
+        et_member_name.setOnClickListener{
+            ll_membersList.visibility = GONE
+            btn_add.visibility = GONE
+            btn_add_h.visibility = VISIBLE
+            btn_back.visibility = VISIBLE
+            ll_members_activity.gravity = Gravity.CENTER_HORIZONTAL
+            tv_new_member_title.visibility = GONE
+        }
+
+        et_member_name.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                ll_membersList.visibility = GONE
+                btn_add.visibility = GONE
+                btn_add_h.visibility = VISIBLE
+                btn_back.visibility = VISIBLE
+                ll_members_activity.gravity = Gravity.CENTER_HORIZONTAL
+                tv_new_member_title.visibility = GONE
+            }
+        }
+
+        et_member_name.onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                ll_membersList.visibility = GONE
+                btn_add.visibility = GONE
+                btn_add_h.visibility = VISIBLE
+                btn_back.visibility = VISIBLE
+                ll_members_activity.gravity = Gravity.CENTER_HORIZONTAL
+                tv_new_member_title.visibility = GONE
+            }
+        }
     }
 
     private fun initializeMemberList() {
@@ -42,6 +92,10 @@ class MembersActivity : AppCompatActivity() {
             membersList.add(m)
         }
         memberAdapter.notifyDataSetChanged()
+    }
+
+    private fun disableFullscreen(view: EditText) {
+        view.setImeOptions(view.getImeOptions() or EditorInfo.IME_FLAG_NO_EXTRACT_UI or EditorInfo.IME_FLAG_NO_FULLSCREEN)
     }
 
     fun onColorSelect(view: View){
@@ -75,8 +129,8 @@ class MembersActivity : AppCompatActivity() {
     }
 
     private fun switchViewVisibility(toGone: View, toVisible: View){
-        toGone.visibility = View.GONE
-        toVisible.visibility = View.VISIBLE
+        toGone.visibility = GONE
+        toVisible.visibility = VISIBLE
     }
 
     fun onAddBtnPressed(view: View){
@@ -86,12 +140,30 @@ class MembersActivity : AppCompatActivity() {
             val result: Long = membersDB.create(newMember)
             if(result > 0){
                 Log.i(Util.LOG_KEY, "insert into members database successful.\n(MembersActivity)")
+                Toast.makeText(this, "$memberName was added as a member.", Toast.LENGTH_SHORT).show()
+                et_member_name.text.clear()
+                readFromDatabase()
             }else{
                 Log.e(Util.LOG_KEY, "failure in adding member to database.\n(MembersActivity)")
             }
         }else{
-            Toast.makeText(this, "Please insert a name.", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Please insert a name.", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun onBackBtnPressed(View: View){
+        ll_membersList.visibility = VISIBLE
+        btn_add.visibility = VISIBLE
+        tv_new_member_title.visibility = VISIBLE
+        btn_add_h.visibility = GONE
+        btn_back.visibility = GONE
+        ll_members_activity.gravity = Gravity.CENTER
+        hideKeyboard()
+    }
+
+    fun hideKeyboard() {
+        val imm: InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
     }
 
     private fun blankName() = et_member_name.text.isEmpty()
--- app/src/main/java/com/guilhermecallandprojects/projectmanager/utils/Util.kt
+++ app/src/main/java/com/guilhermecallandprojects/projectmanager/utils/Util.kt
@@ -1,7 +1,14 @@
 package com.guilhermecallandprojects.projectmanager.utils
 
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
+
 class Util {
     companion object{
         const val LOG_KEY = "projectmanagerapp"
     }
+
+
+
 }
